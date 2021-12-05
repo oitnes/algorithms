9cbf804dfbc4226c5e405c7219903b366482259a
Siarhei Sadouski <s.sadovski.z@gmail.com>
2021-12-05 16:16:05 +0300

    Rework merge sort to algorithm without end mark at sub-array.

--- src/sort/merge_sort.hpp
+++ src/sort/merge_sort.hpp
@@ -6,26 +6,19 @@
 namespace merge_sort {
 
     namespace {
+
         // q - separator between two sub arrays
         template<typename T>
         void merge(std::vector<T> &input, const std::function<bool(T, T)> &comparator, int p, int q, int r) {
-            auto sep_value = std::numeric_limits<T>::max();
-            if (comparator(sep_value, std::numeric_limits<T>::min()))
-                sep_value = std::numeric_limits<T>::min();
-
-            std::vector<T> L{input.begin() + p, input.begin() + q + 1};
-            L.push_back(sep_value);
-            auto right_end = input.end();
-            if (r < input.size())
-                right_end = input.begin() + r + 1;
-            std::vector<T> R{input.begin() + q + 1, right_end};
-            R.push_back(sep_value);
+            std::vector<T> left_part{input.begin() + p, input.begin() + q + 1};
+            const auto right_end = (r < input.size()) ? (input.begin() + r + 1) : input.end();
+            std::vector<T> right_part{input.begin() + q + 1, right_end};
 
             for (int k = p, i = 0, j = 0; k <= r; k++) {
-                if (comparator(L[i], R[j])) {
-                    input[k] = L[i++];
+                if ((left_part.size() > i) && (right_part.size() > j)) {
+                    input[k] = (comparator(left_part[i], right_part[j])) ? left_part[i++] : right_part[j++];
                 } else {
-                    input[k] = R[j++];
+                    input[k] = (left_part.size() > i) ? left_part[i++] : right_part[j++];
                 }
             }
         }
@@ -44,7 +37,7 @@ namespace merge_sort {
 
     template<typename T>
     void sort(std::vector<T> &input, const std::function<bool(T, T)> &comparator) {
-        return merge_sort(input, comparator, 0, input.size()-1);
+        return merge_sort(input, comparator, 0, input.size() - 1);
     }
 
     template<typename T, typename F>
